9b49f4dc72721800926ccc4cf28b449d88810221
XsNIPER50 <jasont.stevens2019@gmail.com>
2026-01-29 19:14:40 -0500

    Fix Team Logo Discord Links

--- utils/team_info.py
+++ utils/team_info.py
@@ -1,97 +1,97 @@
 TEAM_INFO = {
     "Arctic Assassins": {
         "color": 0x00FFFF,  # Cyan / Ice blue
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146906743898212/Arctic_Assassins.png?ex=6932ecc2&is=69319b42&hm=1498f577d77aa02cf0817aec18757bd985552b4fc4792ac3359b180a93651913&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146906743898212/Arctic_Assassins_Logo_New.png?ex=697cc0c2&is=697b6f42&hm=1ae8f4914e042a01c813ebce2c1eae71c0a07ec0bbe01e0e2b88531e27ef04ed&",
         "emoji": "Arctic_Assassins",
         "id": 847658229278048278
     },
     "Armada": {
         "color": 0x12086F,  # Blue
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146907104612482/Armada.png?ex=6932ecc2&is=69319b42&hm=70b1da9e7d5be00d6ef7b7e7bb2fff75ebeeaa045148578afdeb939a6bb1aeea&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146907104612482/Armada_Logo_New.png?ex=697cc0c2&is=697b6f42&hm=d129d306639525adb07fa348846ef6560403266bd0610417cba4eb1e3674ea50&",
         "emoji": "Armada",
         "id": 1100525231250092063
     },
     "Astro": {
         "color": 0x12086F,  # Blue
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146907469779028/Astro_Gaming.png?ex=6932ecc2&is=69319b42&hm=8aebda0df627ba5b35993795096c790d709543bd7e9785cff7fef35a793b1424&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146907469779028/Astro_Gaming_Logo_New.png?ex=697cc0c2&is=697b6f42&hm=4207fbf3f70ca58b0105f751c845de5303b72c1629d67d7c4244236595f80425&",        
         "emoji": "Astro_Gaming",
         "id": 963375539773661184
     },
     "Bleacher Boys": {
         "color": 0x12086F,  # Blue
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146907872165928/Bleacher_Boys.png?ex=6932ecc2&is=69319b42&hm=bbbd2a7bbe78b44e35e83e1677a9681f85f904cdec6fd4572b7a93223e9561d1&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146907872165928/Bleacher_Boys_Logo_New.png?ex=697cc0c2&is=697b6f42&hm=1bb88da297ed80b5aef91e4acdbe68efe856dd5d8860543fc1b08473bb790a38&",
         "emoji": "Bleacher_Boys",
         "id": 775160525645938739
     },
     "Clarity United": {
         "color": 0xFF0000,  # Red
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146908304183367/Clarity_United.png?ex=6932ecc3&is=69319b43&hm=052704c567bad786bd0fd68385bc616c95b5263f80772f230e68e062ee254dd3&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146908304183367/Clarity_United_Logo_New.png?ex=697cc0c3&is=697b6f43&hm=71a27a95018252173d6db86ef56a9b6d454217f68b44bca68d301e74560fe5ea&",
         "emoji": "Clarity_United",
         "id": 775160383673466902
     },
     "Cyclones": {
         "color": 0x808080,  # Gray
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146915002748928/Cyclones.png?ex=6932ecc4&is=69319b44&hm=9cbbaac75ca1b2d66bdf42d8f6522fc7420bf78deb1916edf27c272c24e60b50&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146915002748928/Cyclones_Logo_New.png?ex=697cc0c4&is=697b6f44&hm=48db44949b98388a21ed980e002ed22641b8c85f36a4c3c1333e0c5ed232f00c&",
         "emoji": "Cyclones",
         "id": 963375716571955240
     },
     "Elite Ink": {
         "color": 0xB39EB5,  # Pastel Purple
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146915514187917/Elite_Ink.png?ex=6932ecc4&is=69319b44&hm=2dd444e26df0851844fa295a7a3f9fd049a73eaa1b28a9102929286715045661&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146915514187917/Elite_Ink_Logo_New.png?ex=697cc0c4&is=697b6f44&hm=63a315b0f6b73e1d9e1b315180240bff974d1c6e464020e471803d146f73088c&",
         "emoji": "Elite_Ink",
         "id": 1099489021832482816
     },
     "Hammerheads": {
         "color": 0xFFED29,  # Yellow
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146990873510028/HammerHeads.png?ex=6932ecd6&is=69319b56&hm=fa950d6fd82290a3b35b08368a79af8664b78e1d9a05547203775e00a00dc0bd&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146990873510028/HammerHeads_Logo_New.png?ex=697cc0d6&is=697b6f56&hm=74add3be95a68ea76b1a8628daaae6c5048f2781b0433b37928876eb3f5270af&",
         "emoji": "HammerHeads",
         "id": 775159655051427881
     },
     "Kings": {
         "color": 0x4CBB17,  # Green
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146991838072933/Kings.png?ex=6932ecd6&is=69319b56&hm=1154f537ba520d7704ddbceeaeff243e8b6f380a5fbde9a25998d7281892a309&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146991838072933/Kings_Logo_New.png?ex=697cc0d6&is=697b6f56&hm=497358213fddba253677b71c3cb38200493f2237cc09fefa7a1ba69c270aee57&",
         "emoji": "Kings",
         "id": 775160453490802708
     },
     "Mustangs": {
         "color": 0xED820E,  # Orange
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146992215425195/Mustangs.png?ex=6932ecd7&is=69319b57&hm=125b9327fdc9f372a89dba25df85aea5a54e57f7628206fbe5debefa3e661b38&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146992215425195/Mustangs_Logo_New.png?ex=697cc0d7&is=697b6f57&hm=1fbe37d4f7294908f3a5e9e51bf396cb4a87027e4d518949801c1dcb88003851&",
         "emoji": "Mustangs",
         "id": 1100527145824026626
     },
     "Rangers": {
         "color": 0x8951292,  # Brown
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146992534458553/Rangers.png?ex=6932ecd7&is=69319b57&hm=54dd3b2fc6210fe3d6a46391f5808d889f163d403ddfc9b1a23255f5b6fc2f71&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146992534458553/Rangers_Logo_New.png?ex=697cc0d7&is=697b6f57&hm=66764ada79cfe1051d5b83975d3f671ae3b31d426915ff553c45bbd1bda89b66&",
         "emoji": "Rangers",
         "id": 963375377345028106
     },
     "Sappers": {
         "color": 0xB39EB5,  # Pastel Purple
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146992848900259/Sappers.png?ex=6932ecd7&is=69319b57&hm=a9eeb0d7da656d36d71128bce8ec828c9dee4d46e6ed85d83977789e5b18b7ed&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446146992848900259/Sappers_Logo_New.png?ex=697cc0d7&is=697b6f57&hm=371d875259a4c2328b06878f8bac98658703b3be22ccda2ab02792ff7c43cccf&",
         "emoji": "Sappers",
         "id": 1100526583808282654
     },
     "Spectres": {
         "color": 0x7C0A02,  # Dark Red
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147040324096111/Spectres.png?ex=6932ece2&is=69319b62&hm=e5ea6b2443ab4a6af064f74ba8fbb336489fdd821000e333009dc627a0750ccc&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147040324096111/Spectres_Logo_New.png?ex=697cc0e2&is=697b6f62&hm=32dda8034d76ba11a6d65c3fc44f07781b5e26e1aa664983a2c39e6280b048b1&",
         "emoji": "Spectres",
         "id": 963375885757603861
     },
     "Speed Demons": {
         "color": 0xB39EB5,  # Pastel Purple
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147040626212874/Speed_Demons.png?ex=6932ece2&is=69319b62&hm=0afa2c4d225dcecbd558f881b11c2c8301520cc1b0365391b065d3f920989e4d&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147040626212874/Speed_Demons_Logo_New.png?ex=697cc0e2&is=697b6f62&hm=38498c933b9fb5e520191eb43034f3f03d586c9cc280d05d3f48af46377a5228&",
         "emoji": "Speed_Demons",
         "id": 847553407936888922
     },
     "Three Rocketeers": {
         "color": 0xB39EB5,  # Pastel Purple
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147040987058176/Three_Rocketeers.png?ex=6932ece2&is=69319b62&hm=5c9c48fbca38ce9997173593542bc460d583ad97b619ac2a5367f24304d2c74d&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147040987058176/Three_Rocketeers_Logo_New.png?ex=697cc0e2&is=697b6f62&hm=752f3b7901b6c25254decf07db00c22d1ad59be31456b7199e0d91ffe9fa00ea&",
         "emoji": "Three_Rocketeers",
         "id": 775160106634969138
     },
     "Vibe Clan": {
         "color": 0x00FFFF,  # Cyan / Ice blue
-        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147041666273410/Vibe_Clan.png?ex=6932ece2&is=69319b62&hm=7949fa85c19be3beb8b06dc9115626b3dcf5079d3d3b720ec05f130288797ed2&",
+        "logo": "https://cdn.discordapp.com/attachments/1446133714999316631/1446147041666273410/Vibe_Clan_Logo_New.png?ex=697cc0e2&is=697b6f62&hm=75286f042fb34a120ea983db9e4a825d7e414cdcaf078fd56e8a697d1099757a&",
         "emoji": "Vibe_Clan",
         "id": 775160191594397743
     },
